98d13c21553e979ad4f529833020de519a237d9a
Camel Djoulako <cameldjoulako17@gmail.com>
2019-04-11 10:56:39 +0100

    added files

---
+++ React/AllChallenge.js
@@ -0,0 +1,59 @@
+19
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+};
+
+// change code below this line
+Items.propTypes = {
+  quantity: PropTypes.number.isRequired
+};
+// change code above this line
+
+Items.defaultProps = {
+  quantity: 0
+};
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Items />
+  }
+
+
+20-
+class ReturnTempPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+            { /* change code below this line */ }
+            <p>Your temporary password is: <strong>{this.props.tempPassword}</strong></p>
+            { /* change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class ResetPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          <h2>Reset Password</h2>
+          <h3>We've generated a new temporary password for you.</h3>
+          <h3>Please reset this password from your account settings ASAP.</h3>
+          { /* change code below this line */ }
+          <ReturnTempPassword tempPassword="aaaaaaaa" />
+          { /* change code above this line */ }
+        </div>
+    );
+  }
+};
---
+++ React/firstReactComponent.js
@@ -0,0 +1,17 @@
+class MyComponent extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+        <h1>My First React Component!</h1>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(
+  <MyComponent />,
+  document.getElementById('challenge-node')
+);
